3f78a779db344b28f5e9972531197881676d1f23
lukesahlin <luke@sahlins.net>
2023-01-31 15:04:28 -0800

    vector add

--- main.cpp
+++ main.cpp
@@ -1,6 +1,10 @@
 #include <iostream>
+#include <vector>
 
 int main() {
+    std::vector<int> myVec;
     std::cout << "hello world";
-    
+    for(int i =0; i < 100; i++){
+        myVec.push_back(i);
+    }
 }
